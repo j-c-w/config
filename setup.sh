605e148d5c28c362a35a22d619eaf524a86d65b9
Jackson Woodruff <jacwoo01@moonshot-dsg-10.cambridge.arm.com>
2017-06-30 09:24:18 +0100

    Fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,49 +1,42 @@
 #!/bin/sh
 # Run this from within the config directory
 
-if [ "$#" -ne 1]; then
+if [ "$#" -ne 1 ]; then
 	echo "Usage: $0 Home_Directory" >&2
 	exit 1
 fi
 
-if [ ! -d "$1$" ]; then
+if [ ! -d "$1" ]; then
 	echo "Home directory must exist" >&2
 	exit 1
 fi
 
 # Need pip and git installed:
-if [ command -v pip > /dev/null 2>&1 ]
+if [ command -v pip > /dev/null 2>&1 ]; then
 	echo "pip not installed. Run 'sudo apt-get install pip' "
 	exit 1
 fi
 
-if [command -v git > /dev/null 2>&1 ]
+if [command -v git > /dev/null 2>&1 ]; then
 	echo "git not installed. Run 'sudo apt-get install git' "
 	exit 1
 fi
-
-ln -s .vimrc $1/.vimrc 
-ln -s .vimrc_additions $1/.vimrc_additions
-ln -s .zshrc $1/.zshrc
-
-echo "-------------------------------"
-echo "Config files linked"
-echo "-------------------------------"
-
 # Now, install oh-my-zsh
-$config_directory = pwd
+config_directory=$(pwd)
 cd $1
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 cd .oh-my-zsh/plugins
+echo $(pwd)
 
 echo "------------------------------"
 echo "ohmyzsh installed"
 echo "------------------------------"
 
 # Pull the plugins
-git pull https://github.com/zsh-users/zsh-syntax-highlighting
-git pull https://github.com/zsh-users/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions
 
+echo $(pwd)
 
 echo "------------------------------"
 echo "ohmyzsh plugins installed"
@@ -58,6 +51,19 @@ echo "------------------------------"
 echo "Vundle installed"
 echo "------------------------------"
 
+rm .vimrc
+rm .vimrc_additions
+rm .zshrc
+
+ln -s $config_directory/.vimrc .vimrc 
+ln -s $config_directory/.vimrc_additions .vimrc_additions 
+ln -s $config_directory/.zshrc .zshrc
+
+echo "-------------------------------"
+echo "Config files linked"
+echo "-------------------------------"
+
+
 # Finally, install powerline:
 pip install --user git+git://github.com/Lokaltog/powerline
 
@@ -70,5 +76,8 @@ mkdir -p ~/.config/fontconfig/conf.d/ && mv 10-powerline-symbols.conf ~/.config/
 echo "------------------------------"
 echo "powerline installed"
 echo "------------------------------"
+
+# Now install the vim plugins:
+vim -E -c BundleInstall -c qall
 
 echo "Done!"
