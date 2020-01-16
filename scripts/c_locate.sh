ddda999557c2fb6639dcd9afcad469e6767571d1
Jackson Woodruff <J.C.Woodruff@sms.ed.ac.uk>
2020-01-16 17:28:25 +0000

    Add scripts using fzf for faster directory and file finding

---
+++ scripts/c_locate.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function is_dir() {
+	set -x
+	while read data; do
+		if [[ -d $data ]]; then
+			echo $data
+		fi
+	done <<< $(locate  -r "$@")
+}
+
+if [[ $# == 0 ]]; then
+	args=".*"
+else
+	args="$@"
+fi
+
+is_dir "$args" | fzf
